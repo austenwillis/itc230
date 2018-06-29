89b7c56f98d991fb225edcc2003ead85d69c5216
EC2 Default User <ec2-user@ip-172-31-16-228.us-east-2.compute.internal>
2018-06-29 01:51:07 +0000

    add package.json

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var http = require("http"); 
+var http = require("http"),fs = require('fs');
+
+
+function serveStatic(res, path, contentType, responseCode){
+  if(!responseCode) responseCode = 200;
+  console.log(__dirname + path)
+  fs.readFile(__dirname + path, function(err, data){
+      if(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+      }
+      else{
+        res.writeHead(responseCode, {'Content-Type': contentType});
+        res.end(data);
+      }
+  });
+}
+
+
+
 http.createServer(function(req,res) {
   var path = req.url.toLowerCase();
   switch(path) {
     case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
+      serveStatic(res, '/public/home.html', 'text/html');
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
