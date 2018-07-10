cfa72a7b3ac067606bd7846601931273fa4f7aa1
EC2 Default User <ec2-user@ip-172-31-16-228.us-east-2.compute.internal>
2018-07-10 00:46:27 +0000

    add week2  modular assignment

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var http = require("http"),fs = require('fs');
+var http = require("http"),fs = require('fs'),qs = require("querystring");
+
+var movies = require("./lib/movies.js");
 
 
 function serveStatic(res, path, contentType, responseCode){
@@ -19,7 +21,10 @@ function serveStatic(res, path, contentType, responseCode){
 
 
 http.createServer(function(req,res) {
-  var path = req.url.toLowerCase();
+  let url = req.url.split("?"); 
+  let query = qs.parse(url[1]);
+  let path = url[0].toLowerCase();
+
   switch(path) {
     case '/':
       serveStatic(res, '/public/home.html', 'text/html');
@@ -27,6 +32,24 @@ http.createServer(function(req,res) {
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('About page');
+      break;
+    case '/get':
+      let found = movies.get(query.title); // get movies object
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let results = (found) ? JSON.stringify(found) : "Not found";
+      res.end('Searching for ' + query.title + ":\n" + results);
+      break;
+     case '/delete':
+     let deleted = movies.delete(query.title); 
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let results2 = (deleted.deleted) ? ' was deleted' : " not deleted";
+      res.end('delete \n' + query.title + results2);
+      break;
+   case '/add':
+      let added = movies.add(query.title);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let results3 = (added.added) ? ' was added' : " not added";
+      res.end('add movie \n' + query.title + results3 );
       break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
