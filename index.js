41d4425fe8697e9735eba12013cb484ce510bb62
EC2 Default User <ec2-user@ip-172-31-16-228.us-east-2.compute.internal>
2018-07-17 02:18:40 +0000

    add assignment 3

--- index.js
+++ index.js
@@ -1,59 +1,54 @@
-var http = require("http"),fs = require('fs'),qs = require("querystring");
+'use strict'
+let movies = require("./lib/movies.js");
+const express = require("express");
+const app = express();
 
-var movies = require("./lib/movies.js");
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(require("body-parser").urlencoded({extended: true})); // parse form submissions
+
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html'}));
+app.set("view engine", ".html");
+
+// send static file as response
+app.get('/', (req, res) => {
+ res.type('text/html');
+ res.sendFile(__dirname + '/public/home.html'); 
+});
+
+// send plain text response
+app.get('/about', (req, res) => {
+ res.type('text/plain');
+ res.send('About page');
+});
+
+app.get('/delete', (req, res) => {
+  let deleted = movies.delete(req.query.title); 
+  res.render("delete",{title: req.query.title, deleted: deleted});
+});
 
 
-function serveStatic(res, path, contentType, responseCode){
-  if(!responseCode) responseCode = 200;
-  console.log(__dirname + path)
-  fs.readFile(__dirname + path, function(err, data){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      }
-      else{
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
-  });
-}
+
+app.post('/get', (req, res) => {
+  console.log(req.body);
+  let found = movies.get(req.body.title); 
+  res.render("details",{title: req.body.title, found: found});
+});
 
 
 
-http.createServer(function(req,res) {
-  let url = req.url.split("?"); 
-  let query = qs.parse(url[1]);
-  let path = url[0].toLowerCase();
 
-  switch(path) {
-    case '/':
-      serveStatic(res, '/public/home.html', 'text/html');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    case '/get':
-      let found = movies.get(query.title); // get movies object
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results = (found) ? JSON.stringify(found) : "Not found";
-      res.end('Searching for ' + query.title + ":\n" + results);
-      break;
-     case '/delete':
-     let deleted = movies.delete(query.title); 
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results2 = (deleted.deleted) ? ' was deleted' : " not deleted";
-      res.end('delete \n' + query.title + results2);
-      break;
-   case '/add':
-      let added = movies.add(query.title);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results3 = (added.added) ? ' was added' : " not added";
-      res.end('add movie \n' + query.title + results3 );
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+// define 404 handler
+app.use( (req,res) => {
+ res.type('text/plain'); 
+ res.status(404);
+ res.send('404 - Not found');
+});
+
+
+
+
+app.listen(app.get('port'), () => {
+ console.log('Express Started'); 
+});
